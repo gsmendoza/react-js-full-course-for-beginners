1fe25b6c9ed8ca074071f822184e7041d3cf44be
George Mendoza <gsmendoza@gmail.com>
2024-04-19 17:49:37 +0800

    Shorten method names

--- tutorial-01/src/ItemListItem.jsx
+++ tutorial-01/src/ItemListItem.jsx
@@ -1,7 +1,7 @@
 import { FaTrashAlt } from 'react-icons/fa';
 
 const ItemListItem = ({ items, setItems, item }) => {
-  const handleItemCheck = (id) => {
+  const handleCheck = (id) => {
     const updatedItems = items.map(
       (item) => item.id === id ? { ...item, checked: !item.checked } : item
     );
@@ -10,7 +10,7 @@ const ItemListItem = ({ items, setItems, item }) => {
     localStorage.setItem('groceriesList', JSON.stringify(updatedItems));
   }
 
-  const handleItemDelete = (id) => {
+  const handleDelete = (id) => {
     const updatedItems = items.filter((item) => item.id !== id);
 
     setItems(updatedItems);
@@ -21,19 +21,19 @@ const ItemListItem = ({ items, setItems, item }) => {
     <li key={item.id} className="item">
       <input
         type="checkbox"
-        onChange={() => { handleItemCheck(item.id)}}
+        onChange={() => { handleCheck(item.id)}}
         checked={item.checked}
       />
 
       <label
         style={ item.checked ? { textDecoration: 'line-through' } : null }
-        onDoubleClick={() => { handleItemCheck(item.id)}}
+        onDoubleClick={() => { handleCheck(item.id)}}
       >
         {item.description}
       </label>
 
       <FaTrashAlt
-        onClick={() => { handleItemDelete(item.id)}}
+        onClick={() => { handleDelete(item.id)}}
         role="button"
         tabIndex="0"
       />
